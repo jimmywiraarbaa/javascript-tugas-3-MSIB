195418fcbc19f89fcfd9492475eb1fbc6812644e
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-03-15 15:41:00 +0700

    tugas selesai

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function dataPerson() {
-    let forms = document.getElementById('form');
-    let nama = forms.namespaceURI.value;
+    let form = document.getElementById("formulir");
+    let name = form.name.value;
+    let job = form.job.value;
+    let hobby = form.hobby.value;
+    let greeting = 'Halo ' + name + '<br> Semangat menjadi ' + job + '<br>Jika lelah jangan lupa ' + hobby;
+    let empty = "Mohon maaf anda belum mengingisi formulir";
+    let hasil = (name && job && hobby != '') ? greeting : empty;
 
+    document.getElementById('hasil').innerHTML = hasil;
 }
