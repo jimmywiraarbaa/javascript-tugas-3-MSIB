966298bde52bf86646e3c99205ee61f615a777f3
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-03-15 16:00:35 +0700

    styling tipis tipis

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function dataPerson() {
     let job = form.job.value;
     let hobby = form.hobby.value;
     let greeting = 'Halo ' + name + '<br> Semangat menjadi ' + job + '<br>Jika lelah jangan lupa ' + hobby;
-    let empty = "Mohon maaf anda belum mengingisi formulir";
+    let empty = "Mohon maaf anda belum melengkapi formulir";
     let hasil = (name && job && hobby != '') ? greeting : empty;
 
     document.getElementById('hasil').innerHTML = hasil;
